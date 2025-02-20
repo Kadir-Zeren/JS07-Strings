3a0fefcf726887772a189abfe64626e650936bdc
kadir zeren <k4dzeren@gmail.com>
2025-02-20 18:54:38 +0300

    add string

---
+++ strings.js
@@ -0,0 +1,19 @@
+
+/* -------------------------------------------------------------------------- */
+/*                                   STRINGS                                  */
+/* -------------------------------------------------------------------------- */
+// Bir string, metin veya karakter dizisi olarak adlandırılan verileri temsil eder. JavaScript'te stringler çift tırnak ("), tek tırnak ('), veya backtick (`) içinde tanımlanabilir. 
+
+//? String oluşuturma yöntemleri
+const str1="new string"
+const str2='new string'
+const deger="merhaba"
+const str3=`new string ${deger}`   // backtickler ile template literal
+const str4=new String("new string")
+console.log(str1)
+console.log(str2)
+console.log(str3)
+console.log(str4)
+console.log("🎈🎆🎇🧨✨")
+console.log("©")
+/* -------------------------------------------------------------------------- */
