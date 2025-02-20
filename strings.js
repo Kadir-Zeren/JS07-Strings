3d69bfb728c53d12f501dcd639c750d4eedbf3fd
kadir zeren <k4dzeren@gmail.com>
2025-02-20 20:15:04 +0300

    strings ended

--- strings.js
+++ strings.js
@@ -117,3 +117,186 @@ console.log(emoji.length)
 // trim()	            Removes whitespace from both ends of a string.
 // valueOf()	        Returns the primitive value of a String object.
 /* -------------------------------------------------------------------------- */
+//! String Birleştirme
+
+const name="John"
+const surname="Due"
+const job="Developer"
+
+// + 
+console.log("Our customer's name: "+name+" surname:"+surname+" and his job:"+job)
+console.log(`our customers's name:${name} surname:${surname} and his job: ${job}`)
+const fullname=name.concat(surname)
+console.log(fullname)
+
+// toLowerCase()
+// console.log(fullname.toLowerCase())
+
+// const giris="clarusway"
+// const girisKodu=prompt("Giriş kodunu girin:")
+// if (giris===girisKodu.toLowerCase()){
+//     console.log("Girişiniz onaylandı")
+// }
+// else{
+//     console.log("giriş hatalı")
+// }
+console.clear()
+// toUpperCase()  Büyük harfe çevirir
+
+const statement="Güzel bir gün, kahve ve hurma ile iyi gidiyor"
+console.log(statement.toUpperCase())
+console.log(statement)
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+//!                              String PARÇALAMA                             */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                   split()                                  */
+/* -------------------------------------------------------------------------- */
+//split parçaladığı bölümleri diziye çevirir.
+// str.split(seperator) => seperator bir ayraç, "" , " " , ", ", "/"
+
+let text="Clarusway It bootcamp"
+console.log(text.split(""))
+const splittedText=text.split(" ")
+console.log(splittedText)
+console.log(splittedText[2])
+
+for (let i=splittedText.length-1;i>=0;i--){
+    console.log(splittedText[i])
+}
+
+let months="Jan / Feb / Mar / Apr / May / Jun / Jul / Aug / Sep / Oct / Nov / Dec"
+
+console.log(months.toUpperCase().split("/"))
+
+let  liste="Harry Trump ;Fred Barney; Helen Rigby ; Bill Abel ;Chris Hand"
+let yeniListe=liste.split(";")
+console.log(yeniListe[2])
+
+//! DİKKAT JOİN ve REVERSE string metodu değildir ama splitle çok kullanılır
+
+let cumle="Merhaba Değerli Cohort 16 sakinleri"
+// bu cümleyi terseten string olarak yazın
+let yeniCumle=cumle.split(" ")
+console.log(yeniCumle)
+console.log(yeniCumle[2])
+// reverse tersten yazdırma  - Array metodu
+console.log(yeniCumle.reverse().join(" - "))
+
+// Join - array metodu
+
+// polindrom kelime : Girilen kelime polindrom mudur
+// let kelime=prompt("Kelime Girin:")    
+// let tersten=kelime.split("").reverse().join("")
+// console.log(tersten)
+
+// kelime===tersten ? console.log("polindrom") : console.log("polindrom değil")
+
+/* -------------------------------------------------------------------------- */
+//!Slice 
+//? String içinde bir bölümü almak için kullanılır. (immmutable, kalıcı değişiklik olmaz)
+// Slice(başlangıç index numarası, bitiş index numarası(bu numara dahil değildir))
+
+let ataSoz="Oku da adam ol"
+console.log(ataSoz.slice(7,11))
+console.log(ataSoz.slice(7))
+
+// tersten de erişim sağlanabilir : negatif değer kullanılır
+console.log(ataSoz.slice(-6,-2))
+
+//! substring(başlangıç index numarası,bitiş index numarası) 
+// slicedan tek farkı negatif değer kullanılmaz
+console.log(ataSoz.substring(3,8))
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                        //!Stringde Değişiklik yapma                        */
+/* -------------------------------------------------------------------------- */
+// ! replace(aranan,değişirilecek) - ReplaceAll( aranan,değişirilecek)
+let ozlusoz="Tecrübe tarak gibidir; ama hayat insana kel olduğu zaman verir."
+
+console.log(ozlusoz.replace("kel","saçlı"))
+
+let variable="kullanıcı adı"
+let yeni=variable.replace(" ","_")
+console.log(yeni)
+
+let yeniDeger=yeni.replaceAll("a","â")
+console.log(yeniDeger)
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                      //!String içinde Arama işlemleri                      */
+/* -------------------------------------------------------------------------- */
+// includes, indexOf, search ,match() metodlarını kullanabiliriz
+
+//! includes()   ... yı içeriyor mu -> true yada false bir değer döndürür.caseSensitive (küçük büyük harfe duyarlı) bir özelliktir.
+
+console.log(`ozlusozde tarak kelimesi geçiyor mu:${ozlusoz.includes("tarak")}`)
+
+let url="https://clarusway.com"
+
+let msg= url.includes("https") ? "Bu site güvenilir" :" Bu site güvenli değil"
+
+console.log(msg)
+
+let email="helenclarusway.com"
+
+msg=email.includes("@") ? "Mail adresi geçerli" : "Mail adresi geçersiz"
+console.log(msg)
+
+//!indexOf (aranacak metin)
+//? Bir karakter yada karakter grubumun kaçıncı sırada olduğunu yani index numarasını verir. 
+
+console.log(email.indexOf("com"))
+
+//?Eğer o karakter yoksa olmadığını -1 değeri vererek gösterir. Büyük küçük harfe duyarlıdır. Sadece ilk gördüğünün index numarasını verir
+
+console.log(email.indexOf("edu"))
+
+msg=email.indexOf("edu")=== -1 ? "Bu bir eğitim sitesi değildir" : " Bu bir eğitim sitesidir"
+console.log(msg)
+
+//! search()
+
+// bir string içindeki aranan elemanda ilk bulduğunun index numarasını yazar. Bulamazsa -1 dönderir.
+
+let metin1="Clarusway it bootcamp IT .Clarusway develop you IT field"
+
+console.log(metin1.search("IT"))
+console.log(metin1.search("it"))
+
+// Regex - regular Expression 
+//   / ile ifade edilir
+//  /g => global , bütün cümle içinde uygula
+// /i => case sensitive özlliğini kaldır.Küçük büyük olmasına bakmadan bul
+console.log(metin1.search(/[aeiıoöuü]/ig))
+console.log(metin1.replace(/IT/gi,"Communication"))
+
+//! Match 
+// string içinde aranan metin bulunur ve bir dizi dönderir.
+// Regex ifadeler de kullanılabilir
+
+let metin2="Bugün gerçekten çok güzel , hafif serin  bugün"
+console.log(metin2.match(/bugün/gi))
+
+let değistirilmisMetin=metin2.match(/[e]/gi)
+console.log(değistirilmisMetin.length)
+
+
+
+// trim ,startsWith , endsWith
+let sentence1="     Clarusway  "
+console.log(sentence1.trim())
+
+metin="Clarusway"
+console.log(metin.startsWith("c"))
+console.log(metin.endsWith("y"))
+
+
+
+
+
+
+
